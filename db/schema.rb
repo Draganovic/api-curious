6cc7787198793a6f470a084bbda29ce4dd6f2ce7
Admir Draganovic <admir.draganovic@gmail.com>
2016-05-24 20:40:19 -0600

    Add logout
    Add attributes to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523230655) do
+ActiveRecord::Schema.define(version: 20160525005337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20160523230655) do
     t.datetime "updated_at", null: false
     t.string   "uid"
     t.string   "provider"
+    t.string   "name"
+    t.integer  "following"
+    t.integer  "followers"
   end
 
 end
